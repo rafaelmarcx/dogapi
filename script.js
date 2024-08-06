a078913b53ec117e788d51f0e8133f63d17fbcc4
rafaelmarcx <kiba.games123@gmail.com>
2024-08-06 16:11:29 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('fetchButton').addEventListener('click', fetchDogImages);
+
+function fetchDogImages() {
+    fetch('https://api.thedogapi.com/v1/images/search?limit=10')
+    .then (response => response.json())
+    .then(data => {
+        const gallery = document.getElementById('gallery');
+        gallery.innerHTML = '';
+        data.forEach(dog => {
+            const img = document.createElement('img');
+            img.src = dog.url;
+            gallery.appendChild(img);
+        });
+            
+        
+    })
+    .catch(error => console.error ('Error fetching dog images' , error));
+}
